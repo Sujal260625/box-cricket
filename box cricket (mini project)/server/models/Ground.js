47f0e669546c767a224216d209f47a1b72ce2538
Sujal263-Patel <sujal.patel38833@gmail.com>
2026-04-07 22:00:55 +0530

    remove build and project folder

--- box cricket (mini project)/server/models/Ground.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const GroundSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    status: { type: String, enum: ['active', 'maintenance', 'occupied'], default: 'active' },
-    color: { type: String, default: 'bg-green-500' }
-}, { timestamps: true });
-
-module.exports = mongoose.model('Ground', GroundSchema);
